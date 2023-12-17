114816e23ec53f05feb13279bf6d013c7d4a816d
tinatemishova <tina.temishova@gmail.com>
2023-12-16 22:49:15 -0700

    first commit

---
+++ src/main/java/ecomarket/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package ecomarket.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
