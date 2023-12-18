a150d75c802dedf97d99aa0106bcc908c8f006fe
tinatemishova <tina.temishova@gmail.com>
2023-12-17 21:23:02 -0700

    testing

--- src/main/java/ecomarket/demo/controller/MainController.java
+++ src/main/java/ecomarket/demo/controller/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
     public String welcome() {
         return "welcome";
     }
+
+    @GetMapping("/products")
+    public String products() {
+        return "products";
+    }
 }
